faeb08af4a640e3db8fccb947007b9242a4694db
Lucas Martim <luomartim@gmail.com>
2023-03-27 18:58:57 -0300

    testing diagrams

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,3 +33,8 @@ html_theme = 'sphinx_rtd_theme'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
+
+from N2G import drawio_diagram
+
+diagram = drawio_diagram()
+diagram.from_file("./diagramas/esquematico_painel_solar.drawio")
